d841c9ea70c35d977e7f97f34f57ef4b85a682cf
Nils Henrik Hals <nilshenrik@minorplanet.no>
2015-10-24 03:27:15 +0200

    dawdawdawdawdawd

--- AndroidChat/app/src/main/java/com/firebase/androidchat/UserPad.java
+++ AndroidChat/app/src/main/java/com/firebase/androidchat/UserPad.java
@@ -4,9 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.support.v7.app.AlertDialog;
+import android.telephony.PhoneNumberFormattingTextWatcher;
 import android.telephony.SmsManager;
 import android.view.*;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class UserPad extends Activity {
 
@@ -17,12 +19,19 @@ public class UserPad extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_pad);
         phoneNumberSMSTextView = (EditText)findViewById(R.id.PhoneNumberSMSEditText);
+        phoneNumberSMSTextView.addTextChangedListener(new PhoneNumberFormattingTextWatcher());
+
     }
 
     public void to_phonemessage(View view)
     {
+        if(phoneNumberSMSTextView.getText() == null || phoneNumberSMSTextView.getText().toString().isEmpty()){
+            Toast.makeText(this, "Please enter a phone number", Toast.LENGTH_LONG);
+            return;
+        }
+
         try {
-            SmsManager.getDefault().sendTextMessage(phoneNumberSMSTextView.getText().toString(), null, "Hello SMS!", null, null);
+            SmsManager.getDefault().sendTextMessage(phoneNumberSMSTextView.getText().toString().trim(), null, "Hello SMS!", null, null);
         } catch (Exception e) {
             AlertDialog.Builder alertDialogBuilder = new
                     AlertDialog.Builder(this);
